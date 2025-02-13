66d18e1abfb22366600e89f227bfc371ca6bdcaf
Robert Subotic <129339144+robertsubotic@users.noreply.github.com>
2025-02-13 18:43:30 +0100

    Update Tab.js

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -4,7 +4,7 @@
  *
  * @param {number} id
  * @param {string} [title='New Tab']
- * @param {string} [url='file://c:/Users/robis/Documents/PROJEKTI/nodejs_browser/src/default_page.html']
+ * @param {string} [url='']
  */
 class Tab {
     constructor(id, title = 'New Tab', url = 'file://c:/Users/robis/Documents/PROJEKTI/nodejs_browser/src/default_page.html') {
